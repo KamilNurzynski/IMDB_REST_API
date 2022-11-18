4d716d621f98e249f0d96ae053eae668d366e7b5
KamilNurzynski <nurzynski.kamil@gmail.com>
2022-11-18 09:39:00 +0100

    function based view serializers

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from watchlist_app import views
+from watchlist_app.api import views
 
 urlpatterns = [
     path('list/', views.movie_list, name='movie_list'),
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
-from watchlist_app.models import Movie
-from django.http import JsonResponse
-
-
-def movie_list(request):
-    movies = Movie.objects.all()
-    print(movies.values())
-    data = {
-        'movies': list(movies.values())
-    }
-    return JsonResponse(data)
-
-
-def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    data = {
-        'title': movie.title,
-        'description': movie.description,
-        'active': movie.active
-    }
-    return JsonResponse(data)
+# from django.shortcuts import render
+# from watchlist_app.models import Movie
+# from django.http import JsonResponse
+#
+#
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     print(movies.values())
+#     data = {
+#         'movies': list(movies.values())
+#     }
+#     return JsonResponse(data)
+#
+#
+# def movie_details(request, pk):
+#     movie = Movie.objects.get(pk=pk)
+#     data = {
+#         'title': movie.title,
+#         'description': movie.description,
+#         'active': movie.active
+#     }
+#     return JsonResponse(data)
